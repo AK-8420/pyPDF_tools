ac9b98f49e4df86af29e007c7045f268c47b2f18
Akari Katsuma <katainu0103@gmail.com>
2022-04-22 00:17:34 +0900

    ArgumentParser追加

--- pdfmerger.py
+++ pdfmerger.py
@@ -1,6 +1,15 @@
+from argparse import ArgumentParser
 import glob
-import os
 import PyPDF4
+
+argparser = ArgumentParser(description='Merge some PDF files.')
+argparser.add_argument('-f', '--files', type=str, nargs='+',
+                        default='*.pdf',
+                        help='Specify filenames of input')
+argparser.add_argument('-o', '--out', type=str,
+                        default='out.pdf',
+                        help='Specify filename of output')
+args = argparser.parse_args()
 
 pdfs = glob.glob("*.pdf")
 merger = PyPDF4.PdfFileMerger()
