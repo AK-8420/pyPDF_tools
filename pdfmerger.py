933c5f21615e19e5fbebf6a15daf01cc1c14ab15
Akari Katsuma <katainu0103@gmail.com>
2022-04-22 00:19:25 +0900

    変数名追加

--- pdfmerger.py
+++ pdfmerger.py
@@ -5,17 +5,19 @@ import PyPDF4
 argparser = ArgumentParser(description='Merge some PDF files.')
 argparser.add_argument('-f', '--files', type=str, nargs='+',
                         default='*.pdf',
+                        dest='pdfs',
                         help='Specify filenames of input')
 argparser.add_argument('-o', '--out', type=str,
                         default='out.pdf',
+                        dest='out_name',
                         help='Specify filename of output')
 args = argparser.parse_args()
 
-pdfs = glob.glob("*.pdf")
+#pdfs = glob.glob("*.pdf")
 merger = PyPDF4.PdfFileMerger()
 
-for p in pdfs:
+for p in args.pdfs:
     merger.append(p)
 
-merger.write("out.pdf")
+merger.write(args.out_name)
 merger.close()
